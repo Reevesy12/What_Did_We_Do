1ce963a953b7841f280ed5da8cc15ac16f8a4e23
Reevesy12 <paulreeves12@gmail.com>
2026-07-20 16:52:14 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -11,7 +11,11 @@ self.addEventListener('install', event => {
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+      Promise.all(
+        keys
+          .filter(k => k.indexOf('what-did-we-do') === 0 && k !== CACHE_NAME)
+          .map(k => caches.delete(k))
+      )
     )
   );
   self.clients.claim();
